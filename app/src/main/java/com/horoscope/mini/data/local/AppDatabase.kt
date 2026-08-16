03e8a5bf1639ba26d17dbc9b6481a18d85f38061
chepikk <chepikk@gmail.com>
2026-08-17 02:23:10 +0700

    Обновлён AppDatabase

--- app/src/main/java/com/horoscope/mini/data/local/AppDatabase.kt
+++ app/src/main/java/com/horoscope/mini/data/local/AppDatabase.kt
@@ -2,12 +2,24 @@ package com.horoscope.mini.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.horoscope.mini.data.local.dao.HoroscopeDao
+import com.horoscope.mini.data.local.dao.PurchaseDao
+import com.horoscope.mini.data.local.dao.UserDao
+import com.horoscope.mini.data.local.entity.HoroscopeEntity
+import com.horoscope.mini.data.local.entity.PurchaseEntity
+import com.horoscope.mini.data.local.entity.UserEntity
 
 @Database(
-    entities = [HoroscopeEntity::class],
+    entities = [
+        UserEntity::class,
+        HoroscopeEntity::class,
+        PurchaseEntity::class
+    ],
     version = 1,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
+    abstract fun userDao(): UserDao
     abstract fun horoscopeDao(): HoroscopeDao
+    abstract fun purchaseDao(): PurchaseDao
 }
