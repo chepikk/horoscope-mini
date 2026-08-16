2c324eeb1f65c58493423854429450fcda357107
chepikk <chepikk@gmail.com>
2026-08-17 04:49:27 +0700

    Фикс импортов в AppDatabase

--- app/src/main/java/com/horoscope/mini/data/local/AppDatabase.kt
+++ app/src/main/java/com/horoscope/mini/data/local/AppDatabase.kt
@@ -2,6 +2,8 @@ package com.horoscope.mini.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.horoscope.mini.domain.model.HoroscopeItem
+import com.horoscope.mini.domain.model.User
 
 @Database(
     entities = [User::class, HoroscopeItem::class],
