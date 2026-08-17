8b9f89cfbe9e86797a6a22ef755e94eb46b5a9da
chepikk <chepikk@gmail.com>
2026-08-17 14:00:18 +0700

    fix: use collectAsState for reactive updates

--- app/src/main/java/com/horoscope/mini/presentation/main/HoroscopeTabs.kt
+++ app/src/main/java/com/horoscope/mini/presentation/main/HoroscopeTabs.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -15,7 +17,7 @@ import com.horoscope.mini.domain.model.HoroscopeItem
 @Composable
 fun HoroscopeTabs(viewModel: HoroscopeViewModel = hiltViewModel()) {
 
-    val horoscopes = viewModel.horoscopes.value
+    val horoscopes by viewModel.horoscopes.collectAsState()
 
     LaunchedEffect(Unit) {
         if (horoscopes.isEmpty()) {
