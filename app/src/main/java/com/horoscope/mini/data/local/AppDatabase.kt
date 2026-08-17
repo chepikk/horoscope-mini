d8af4b9f39bd518258d829ccb3174ab9d8bedb7e
chepikk <chepikk@gmail.com>
2026-08-17 12:29:10 +0700

    Добавлена сущность Purchase в AppDatabase

--- app/src/main/java/com/horoscope/mini/data/local/AppDatabase.kt
+++ app/src/main/java/com/horoscope/mini/data/local/AppDatabase.kt
@@ -3,10 +3,11 @@ package com.horoscope.mini.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.horoscope.mini.domain.model.HoroscopeItem
+import com.horoscope.mini.domain.model.Purchase
 import com.horoscope.mini.domain.model.User
 
 @Database(
-    entities = [User::class, HoroscopeItem::class],
+    entities = [User::class, HoroscopeItem::class, Purchase::class],
     version = 1,
     exportSchema = false
 )
